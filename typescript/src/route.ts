d63814bf066e91b5a7cc1df646b658d92706a392
baguiar-20 <baguiar237@gmail.com>
2022-01-10 16:13:13 -0400

    Atualizar

--- typescript/src/route.ts
+++ typescript/src/route.ts
@@ -1,6 +1,13 @@
 import {Request, Response} from 'express';
+import createUser from './services/CreateUser'
 
 
 export function Hello(request: Request, response: Response){
+    const user = createUser({
+        name: 'brenda', 
+        email: 'brenda@gmail.com', 
+        password: '1234',
+        techs: ['Nodejs'],
+    });
     return response.json({message: "coco"});
 }
