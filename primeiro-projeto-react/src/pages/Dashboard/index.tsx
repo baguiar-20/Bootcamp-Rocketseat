c548a4daf0bd6d41cd3eb175184a631b763ee441
baguiar-20 <baguiar237@gmail.com>
2022-01-15 13:48:40 -0400

    Terminando a primeira parte do projeto react

--- primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,11 +1,70 @@
-import React from "react";
+import React, {useState, FormEvent} from "react";
 
-import { Title} from './styles'
+import { Title, Form, Repository} from './styles'
+import LogoImg from '../../assets/logo.svg'
+import {FiChevronRight} from 'react-icons/fi'
+import api from '../../services/api'
 
+interface Repository{
+    full_name: string;
+    description: string;
+    owner:{
+        login: string;
+        avatar_url: string;
+    };
+}
 
 const Dashboard: React.FC = () =>{
+
+    const [newRepo, setNewRepo] = useState('');
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void>{
+        console.log(newRepo);
+        event.preventDefault();
+        const response = await api.get<Repository>(`repos/${newRepo}`)
+        //adicao novo repositorio
+        //consumir api do github
+        //salvar novo repositorio no estado
+        //console.log(response.data);
+        const repository = response.data;
+        setRepositories([...repositories, repository]);
+        setNewRepo('');
+    }
+
+
     return (
+        <>
+        <img src={LogoImg} alt="Github Explorer"/>
         <Title>Explore respositorios no Github</Title>
+
+        <Form onSubmit={handleAddRepository}>
+            <input 
+            value={newRepo}
+            onChange={(e) => setNewRepo(e.target.value)}
+            placeholder="Digite o nome do repositorio"/>
+            <button type="submit">Pesquisar</button>
+        </Form>
+
+        <Repository>
+            {repositories.map(repository=>(
+                <a key={repository.full_name} href="teste">
+                    <img 
+                        src={repository.owner.avatar_url} 
+                        alt={repository.owner.login} />
+                    <div>
+                        <strong>{repository.full_name}</strong>
+                        <p>{repository.description}</p>
+                    </div>
+                <FiChevronRight size={20}/>
+                </a> 
+
+            ))}
+               
+        </Repository>
+
+
+        </> 
     ) 
 
 }
--- primeiro-projeto-react/src/styles/global.ts
+++ primeiro-projeto-react/src/styles/global.ts
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
 
+import githubBackground from '../assets/github-background.svg'
+
 
 export default createGlobalStyle`
     *{
@@ -10,8 +12,18 @@ export default createGlobalStyle`
     }
 
     body{
-        background: #f0f0f5;
-        --webkit-font-smoing: an
+        background: #F0F0F5 url(${githubBackground}) no-repeat 70% top;
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, button{
+        font:16px Roboto, sans-serif;
+    }
+
+    #root{
+        max-width: 960px;
+        margin: 0 auto;
+        padding: 40px 20px;
     }
 
 `;
